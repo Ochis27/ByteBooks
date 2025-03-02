60970f83abd6e90bf0cbd5cc8a50e5389b249c8b
Ochis27 <ochispaulcatalin27@gmail.com>
2025-03-03 00:06:49 +0200

    added middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,14 @@ import express from "express";
 import authRouter from "./routes/auth";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended: false}));
 app.use("/auth", authRouter);
+app.post("/test", (req, res) => {
+  console.log(req.body);
+  res.json({});
+});
 
 const port = process.env.PORT || 8989;
 
