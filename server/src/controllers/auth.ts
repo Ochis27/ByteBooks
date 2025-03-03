9fd31d6a416827b72ecb5b0932ea1e41e6d7eaa3
Ochis27 <ochispaulcatalin27@gmail.com>
2025-03-03 08:21:39 +0200

    zod validation

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,8 +1,10 @@
+import crypto from "crypto";
 import {RequestHandler} from "express";
 
 export const generateAuthLink: RequestHandler = (req, res) => {
   // Generate authentication
   // and send the link to the users email address
+  const randomToken = crypto.randomBytes(36).toString("hex");
   console.log(req.body);
   res.json({ok: true});
 };
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,32 +1,12 @@
 import {generateAuthLink} from "@/controllers/auth";
+import {emailValidationSchema, validate} from "@/middlewares/validator";
 import {Router} from "express";
-import {z} from "zod";
 
 const authRouter = Router();
 
-const schema = z.object({
-  email: z
-    .string({required_error: "Email is missing"})
-    .email("Invalid email address!"),
-});
-
 authRouter.post(
   "/generate-link",
-  (req, res, next) => {
-    const {email} = req.body;
-    // const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$");
-    // if (!regex.test(email)) {
-    //   // sending error response
-    //   return res.status(422).json({error: "Invalid email!"});
-    // }
-    const result = schema.safeParse(req.body);
-    if (!result.success) {
-      console.log(JSON.stringify(result, null, 2));
-      const errors = result.error.flatten().fieldErrors;
-      return res.status(422).json({errors});
-    }
-    next();
-  },
+  validate(emailValidationSchema),
   generateAuthLink
 );
 
